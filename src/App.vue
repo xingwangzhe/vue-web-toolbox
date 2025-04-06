f13df074b84ab65fca5d8f63cd363279524b2364
王兴家 <xingwangzhe@outlook.com>
2025-04-06 20:47:03 +0800

    feat:初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped></style>
