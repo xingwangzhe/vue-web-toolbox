d37583ea45388d5c7e04148a55eeaa5a0f331f58
王兴家 <xingwangzhe@outlook.com>
2025-04-11 21:46:03 +0800

    feat:del

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
